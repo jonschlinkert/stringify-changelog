c0cfe1c1a126d70ea18e20a1314153295bcf4098
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-16 10:09:27 -0400

    clean up

--- test.js
+++ test.js
@@ -11,11 +11,11 @@
 var assert = require('assert');
 var should = require('should');
 var moment = require('moment');
-var changlog = require('./');
+var changelog = require('./');
 
-describe('changlog', function () {
+describe('changelog', function () {
   it('should generate a changelog from a yaml file:', function () {
-    changlog('fixtures/a.yml').should.equal([
+    changelog('fixtures/a.yml').should.equal([
       '**DATE**       **VERSION**   **CHANGES**                  ',
       '* 2016-12-26   v0.1.0        Got stuck in another chimney.',
     ].join('\n'));
@@ -23,7 +23,7 @@ describe('changlog', function () {
 
   it('should generate a changelog from an object:', function () {
     var data = { 'v0.1.0': { date: '2016-12-26', changes: [ 'Got stuck in another chimney.' ] } };
-    changlog(data).should.equal([
+    changelog(data).should.equal([
       '**DATE**       **VERSION**   **CHANGES**                  ',
       '* 2016-12-26   v0.1.0        Got stuck in another chimney.',
     ].join('\n'));
@@ -34,7 +34,7 @@ describe('changlog', function () {
       {date: '2016-12-26', version: 'v0.1.0', changes: [ 'Got stuck in another chimney.' ]
     }];
 
-    changlog(data).should.equal([
+    changelog(data).should.equal([
       '**DATE**       **VERSION**   **CHANGES**                  ',
       '* 2016-12-26   v0.1.0        Got stuck in another chimney.',
     ].join('\n'));
@@ -47,11 +47,10 @@ describe('changlog', function () {
 
     var opts = {};
     opts.dateFn = function (date) {
-    // console.log(arguments)
       return ' * ' + moment(date).format('YYYY-MM-DD');
     };
 
-    changlog(data, opts).should.equal([
+    changelog(data, opts).should.equal([
       '**DATE**       **VERSION**   **CHANGES**                  ',
       '* 2016-12-26   v0.1.0        Got stuck in another chimney.',
     ].join('\n'));
@@ -59,7 +58,7 @@ describe('changlog', function () {
 
   it('should throw an error:', function () {
     (function () {
-      changlog();
+      changelog();
     }).should.throw('helper-changelog cannot find data or a file to read.');
   });
 });
